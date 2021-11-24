85f39ca34a28d341b98c8bc5f5770750a1ce76ba
Adrian Lin <adrianlin@Adrians-MacBook-Pro.local>
2021-11-24 12:31:35 -0800

    particle effects and parallax scrolling

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -4,19 +4,28 @@ using UnityEngine;
 
 public class Shooter : MonoBehaviour
 {
+    [Header("General")]
     [SerializeField] GameObject projectilePrefab;
     [SerializeField] float projectileSpeed = 10f;
     [SerializeField] float projectileLifetime = 5f;
-    [SerializeField] float firingRate = 0.2f;
+    [SerializeField] float baseFiringRate = 0.2f;
 
-    public bool isFiring;
+    [Header("AI")]
+    [SerializeField] bool useAI;
+    [SerializeField] float firingRateVariance = 0f;
+    [SerializeField] float minimumFiringRate = 0.1f;
+
+    [HideInInspector] public bool isFiring;
 
     Coroutine firingCoroutine;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (useAI)
+        {
+            isFiring = true;
+        }
     }
 
     // Update is called once per frame
@@ -51,7 +60,12 @@ public class Shooter : MonoBehaviour
                 rb.velocity = transform.up * projectileSpeed;
             }
             Destroy(instance, projectileLifetime);
-            yield return new WaitForSeconds(firingRate);
+            float timeToNextProjectile = Random.Range(baseFiringRate - firingRateVariance,
+                                        baseFiringRate + firingRateVariance);
+            timeToNextProjectile = Mathf.Clamp(timeToNextProjectile,
+                                            minimumFiringRate,
+                                            float.MaxValue);
+            yield return new WaitForSeconds(timeToNextProjectile);
         }
     }
 }
---
+++ Assets/Scripts/SpriteScroller.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteScroller : MonoBehaviour
+{
+    [SerializeField] Vector2 moveSpeed;
+
+    Vector2 offset;
+    Material material;
+
+    void Awake()
+    {
+        material = GetComponent<SpriteRenderer>().material;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        offset = moveSpeed * Time.deltaTime;
+        material.mainTextureOffset += offset;
+    }
+}
